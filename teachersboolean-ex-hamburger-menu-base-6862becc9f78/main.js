9d53c9130150437dfde4db71fdc2e25509dcc34a
ValleLuca <valle.lucaAb@outlook.it>
2021-12-09 16:26:33 +0100

    Apertura menu riuscita

---
+++ teachersboolean-ex-hamburger-menu-base-6862becc9f78/main.js
@@ -0,0 +1,11 @@
+const element = document.querySelector(".fa-bars");
+
+Element-addEventListener("click", function() {
+    document.querySelector(".hamburger-menu").style.display = "block";
+});
+
+// const element = document.querySelector(".hamburger-menu .close .fa-times");
+
+//Element-addEventListener("click", function() {
+   // document.querySelector(".hamburger-menu").style.display = "none";
+//});
